8fd4cef03fb132ae9c10db13b526cbe22fd35805
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2023-10-04 15:42:02 +0500

    Created and styled contents of page

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const form = document.querySelector('dialog')
+const addBookbtn = document.getElementById('add-book-btn')
+
+addBookbtn.addEventListener('click', function openForm() {
+  form.showModal()
+  
+})
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+
+
+function addBookToLibrary() {
+  // do stuff here
+  
+}
