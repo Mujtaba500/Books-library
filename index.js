b9dd660524dd9661d074a91987d832160d8188d7
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2023-10-05 11:08:09 +0500

    Styled and displayed books using CSS grid

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 
 const form = document.querySelector('dialog')
 const addBookbtn = document.getElementById('add-book-btn')
+const booksHolder = document.getElementById('books-holder')
 
 addBookbtn.addEventListener('click', function openForm() {
   form.showModal()
   
 })
 
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: 'harry potter',
+    author: 'jk rowlin',
+    pages: 200,
+    read: true
+  },
+  {
+    title: 'gk',
+    author: 'yoyo',
+    pages: 97,
+    read: false
+  },
+  {
+    title: 'yaya',
+    author: 'hello',
+    pages: 9,
+    read: true
+  },
+{
+  title: 'yaya',
+    author: 'hello',
+    pages: 9,
+    read: true
+},
+{
+  title: 'yaya',
+    author: 'hello',
+    pages: 9,
+    read: true
+},
+{
+  title: 'yaya',
+    author: 'hello',
+    pages: 9,
+    read: true
+},
+{
+  title: 'yaya',
+    author: 'hello',
+    pages: 9,
+    read: true
+}
+
+];
 
 function Book() {
   // the constructor...
@@ -19,3 +64,18 @@ function addBookToLibrary() {
   // do stuff here
   
 }
+
+function render() {
+  for(let i = 0; i < myLibrary.length; i++){
+  booksHolder.innerHTML += `
+  <div>
+   <p>${myLibrary[i].title}</p>
+   <p>${myLibrary[i].author}</p>
+   <p>${myLibrary[i].pages} pages</p> 
+   <button class="read-btn">Read</button>
+   <button class="remove-btn">Remove</button>
+  </div>
+  `
+  }
+}
+render()
